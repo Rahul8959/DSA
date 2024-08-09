88ed7eebd67b8a912f6073420d0b0e1586deeab2
Rahul8959 <rahulshakya732@gmail.com>
2024-08-09 08:42:10 +0530

    Longest subarray with given sum k

--- 6. Array/Easy/Longest_subarray_with_given_sum_K(positives).py
+++ 6. Array/Easy/Longest_subarray_with_given_sum_K(positives).py
@@ -1,16 +1,45 @@
-def longest_sunarray(arr,n,k):
-    l = 0
-    for i in range(n):
-        s = 0
-        for j in range(i, n):
-            s+=arr[j]
-            if(s==k):
-                l = max(l,j-i+1)
+# Brute Force
+# def longest_subarray(arr,n,k):
+#     l = 0
+#     for i in range(n):
+#         s = 0
+#         for j in range(i, n):
+#             s+=arr[j]
+#             if(s==k):
+#                 l = max(l,j-i+1)
                 
-    return l
+#     return l
+
+#Better
+def longest_subarray(a, n, k):
+
+    preSumMap = {}
+    Sum = 0
+    maxLen = 0
+    for i in range(n):
+        # calculate the prefix sum till index i:
+        Sum += a[i]
+
+        # if the sum = k, update the maxLen:
+        if Sum == k:
+            maxLen = max(maxLen, i + 1)
+
+        # calculate the sum of remaining part i.e. x-k:
+        rem = Sum - k
+
+        # Calculate the length and update maxLen:
+        if rem in preSumMap:
+            length = i - preSumMap[rem]
+            maxLen = max(maxLen, length)
+
+        # Finally, update the map checking the conditions:
+        if Sum not in preSumMap:
+            preSumMap[Sum] = i
+
+    return maxLen
 
 x = input("Enter array items: ")
 arr = [int(i) for i in x.split()]
 n=len(arr)
 k = int(input("Enter K Value: "))
-print("Longest subarray is: ", longest_sunarray(arr,n,k))
+print("Longest subarray is: ", longest_subarray(arr,n,k))
