382e2be3563cb38af7c76e5246348f4f15b831e6
Rahul8959 <rahulshakya732@gmail.com>
2024-08-09 09:11:05 +0530

    longest sunbarray with given sum k

--- 6. Array/Easy/Longest_subarray_with_given_sum_K(positives).py
+++ 6. Array/Easy/Longest_subarray_with_given_sum_K(positives).py
@@ -11,31 +11,51 @@
 #     return l
 
 #Better
-def longest_subarray(a, n, k):
+# def longest_subarray(a, n, k):
 
-    preSumMap = {}
-    Sum = 0
+#     preSumMap = {}
+#     Sum = 0
+#     maxLen = 0
+#     for i in range(n):
+#         # calculate the prefix sum till index i:
+#         Sum += a[i]
+
+#         # if the sum = k, update the maxLen:
+#         if Sum == k:
+#             maxLen = max(maxLen, i + 1)
+
+#         # calculate the sum of remaining part i.e. x-k:
+#         rem = Sum - k
+
+#         # Calculate the length and update maxLen:
+#         if rem in preSumMap:
+#             length = i - preSumMap[rem]
+#             maxLen = max(maxLen, length)
+
+#         # Finally, update the map checking the conditions:
+#         if Sum not in preSumMap:
+#             preSumMap[Sum] = i
+
+#     return maxLen
+
+def longest_subarray(a,n, k):
+    left, right = 0, 0 # 2 pointers
+    Sum = a[0]
     maxLen = 0
-    for i in range(n):
-        # calculate the prefix sum till index i:
-        Sum += a[i]
+    while right < n:
+        # if sum > k, reduce the subarray from left
+        # until sum becomes less or equal to k:
+        while left <= right and Sum > k:
+            Sum -= a[left]
+            left += 1
 
-        # if the sum = k, update the maxLen:
+        # if sum = k, update the maxLen i.e. answer:
         if Sum == k:
-            maxLen = max(maxLen, i + 1)
+            maxLen = max(maxLen, right - left + 1)
 
-        # calculate the sum of remaining part i.e. x-k:
-        rem = Sum - k
-
-        # Calculate the length and update maxLen:
-        if rem in preSumMap:
-            length = i - preSumMap[rem]
-            maxLen = max(maxLen, length)
-
-        # Finally, update the map checking the conditions:
-        if Sum not in preSumMap:
-            preSumMap[Sum] = i
-
+        # Move forward the right pointer:
+        right += 1
+        if right < n: Sum += a[right]
     return maxLen
 
 x = input("Enter array items: ")
