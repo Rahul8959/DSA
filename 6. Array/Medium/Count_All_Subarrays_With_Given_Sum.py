9afd0c1405327d84d31240d7032d6de65aa1b635
Rahul8959 <rahulshakya732@gmail.com>
2024-09-13 08:53:06 +0530

    Count all subarrays with given sum

--- 6. Array/Medium/Count_All_Subarrays_With_Given_Sum.py
+++ 6. Array/Medium/Count_All_Subarrays_With_Given_Sum.py
@@ -1,14 +1,40 @@
-def findAllSubarraysWithGivenSum(arr, s):
-    # Write your code here.
-    n = len(arr)
+from collections import defaultdict
+# Better
+# def findAllSubarraysWithGivenSum(arr, s):
+#     # Write your code here.
+#     n = len(arr)
+#     cnt = 0
+#     ans = 0
+#     for i in range(n):
+#         summ = 0
+#         for j in range(i,n):
+#             summ += arr[j]
+#             if summ==s:
+#                 cnt+=1
+#     return cnt
+
+#Optimized
+def findAllSubarraysWithGivenSum(arr, k):
+    n = len(arr) # size of the given array.
+    mpp = defaultdict(int)
+    preSum = 0
     cnt = 0
-    ans = 0
+
+    mpp[0] = 1 # Setting 0 in the map.
     for i in range(n):
-        summ = 0
-        for j in range(i,n):
-            summ += arr[j]
-            if summ==s:
-                cnt+=1
+        # add current element to prefix Sum:
+        preSum += arr[i]
+
+        # Calculate x-k:
+        remove = preSum - k
+
+        # Add the number of subarrays to be removed:
+        cnt += mpp[remove]
+
+        # Update the count of prefix sum
+        # in the map.
+        mpp[preSum] += 1
+
     return cnt
 
 x = input("Enter the list: ")
